afd636185758471b170fbb64e08cf3b734cc474f
Retro Sapphire <29125087+retro-sapphire@users.noreply.github.com>
2026-02-03 20:51:09 +0530

    Add a render loop thread

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use std::error::Error;
+use crossbeam::channel;
+use crossterm::{
+    ExecutableCommand,
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use invaders::{
+    frame::{self, new_frame},
+    render,
+};
 use rusty_audio::Audio;
+use std::{error::Error, io, thread, time::Duration};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
     audio.add("explode", "sounds/explode.wav");
-    audio.add("lose", "sounds/explode.wav");
+    audio.add("lose", "sounds/lose.wav");
     audio.add("move", "sounds/move.wav");
     audio.add("pew", "sounds/pew.wav");
     audio.add("startup", "sounds/startup.wav");
@@ -12,7 +23,57 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     audio.play("startup");
 
+    // Setup
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Separate thread for render loop
+    let (tx, rx) = channel::unbounded();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game Loop
+    'gameloop: loop {
+        // Per Frame Init
+        let curr_frame = new_frame();
+
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Draw & render
+        let _ = tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
     // Cleanup
+    drop(tx);
+    render_handle.join().unwrap();
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
