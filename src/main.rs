d0e07421eb00a9d9c529aa08e932d088a4eb576e
Retro Sapphire <29125087+retro-sapphire@users.noreply.github.com>
2026-02-03 22:09:35 +0530

    Add a shooting function

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,11 @@ use invaders::{
     render,
 };
 use rusty_audio::Audio;
-use std::{error::Error, io, thread, time::Duration};
+use std::{
+    error::Error,
+    io, thread,
+    time::{Duration, Instant},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -48,8 +52,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Game Loop
     let mut player = Player::new();
+    let mut instant = Instant::now();
     'gameloop: loop {
         // Per Frame Init
+        let delta = instant.elapsed();
+        instant = Instant::now();
         let mut curr_frame = new_frame();
 
         // Input
@@ -58,6 +65,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                 match key_event.code {
                     KeyCode::Left => player.move_left(),
                     KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -66,6 +78,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Updates
+        player.update(delta);
 
         // Draw & render
         player.draw(&mut curr_frame);
